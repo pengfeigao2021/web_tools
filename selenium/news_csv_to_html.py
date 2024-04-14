c4365565a3ce11088ff5d91cc9856440a67f3628
pengfeigao2021 <pengfeigao2021@163.com>
2024-04-13 19:47:26 -0700

    [m] remove dup titles
    Signed-off-by: pengfeigao2021 <pengfeigao2021@163.com>

--- selenium/news_csv_to_html.py
+++ selenium/news_csv_to_html.py
@@ -4,6 +4,21 @@ import glob
 
 DATA_DIR = '/Users/AlexG/data/selenium'
 
+def isduplicate(urls, titles, title, url):
+    for t in titles:
+        if t.lower() == title.lower():
+            return True
+        if t.lower().startswith(title.lower()):
+            return True
+    for u, t in zip(urls, titles):
+        if u.lower() == url.lower():
+            if len(title) < len(t):
+                return True
+            else:
+                return title < t
+    return False
+    
+
 def csv2html(files, html_path):
     news = []
     source_list = []
@@ -11,6 +26,32 @@ def csv2html(files, html_path):
         name = os.path.splitext(os.path.basename(p))[0]
         source_list.append(name)
         df = pd.read_csv(p)
+
+        # remove duplicates
+        dup_count = 0
+        titles = []
+        urls = []
+        valid_indices = {i: True for i in range(len(df))}
+        for i in range(len(df)):
+            title = df.iloc[i]['title']
+            url = df.iloc[i]['url']
+
+            # get other titles
+            other_titles = [
+                df.iloc[j]['title'] 
+                for j, v in valid_indices.items() 
+                if v and j != i]
+            other_urls = [
+                df.iloc[j]['url'] 
+                for j, v in valid_indices.items() 
+                if v and j != i]
+            if isduplicate(other_urls, other_titles, title, url):
+                valid_indices[i] = False
+                dup_count += 1
+                continue
+            titles.append(title)
+            urls.append(url)
+        
         list_content = '''
         <ol>
         {}
@@ -18,13 +59,17 @@ def csv2html(files, html_path):
         '''.format(
             '\n'.join(
                 f'<li><a href={url}>{title}</a></li>' for 
-                title, url in zip(df['title'], df['url'])))
+                title, url in zip(titles, urls)))
 
         news_content = f'''
         <h2 id="{name}">{name}</h2>
         {list_content}
         '''
         news.append(news_content)
+        
+        # duplication count
+        print(f'{name} has {dup_count} duplicates')
+
     content = '''
     <html>
     <head>
